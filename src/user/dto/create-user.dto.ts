2c8c197d8038e94aa016541bfbab3e01b848f592
tc-mamunsyuhada <imam@tokocrypto.com>
2022-04-20 23:13:11 +0700

    add: req body validation of createUser

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,29 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @IsOptional()
   id?: number;
+
+  @IsString()
+  @MaxLength(200)
   user_name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MaxLength(50)
+  @MinLength(6)
   full_name: string;
+
+  @IsString()
+  @MaxLength(32)
+  @MinLength(6)
   password: string;
-  create_at: Date;
-  update_at: Date;
 }
