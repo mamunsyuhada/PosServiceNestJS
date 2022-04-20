61925b67f5d14e97ac7eee9c8e58b503c0f16111
tc-mamunsyuhada <imam@tokocrypto.com>
2022-04-20 17:23:35 +0700

    add: first crud user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { User } from './entities/user.entity';
 @Injectable()
 export class UserService {
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.userRepo.save(createUserDto);
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepo.findOne(id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    updateUserDto.id = id;
+    return this.userRepo.save(updateUserDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.userRepo.findOne(id);
+    return this.userRepo.remove(user);
   }
 }
